006c393ef7010014e6ad1d6639b310b86e88c18b
Luke <lukeleibing@gmail.com>
2024-07-24 15:33:06 +0100

    fixed typo

--- public/cards.js
+++ public/cards.js
@@ -253,14 +253,14 @@ const store = reactive({ //updates the html immediately
     document.getElementById("mainOrSunDialog").showModal()
   },
   inc() {
-    const newPostion = this.curser + 1
+    const newPosition = this.curser + 1
     //this.curser++
     //this.cards = [...this.cards.slice(0, this.curser), {...this.newCard}, ...this.cards.slice(this.curser)]
     this.curser = this.cards.length
     this.cards = [...this.cards, {...this.newCard}]
     let timer = setInterval(() => {
       this.swapCards(this.curser, this.curser -1, false)
-      if (this.curser === newPostion) {
+      if (this.curser === newPosition) {
 	clearInterval(timer)
 	const elements = document.getElementsByClassName("outerMainCard")[this.curser].getElementsByClassName("inner");
 	elements[0].focus()
